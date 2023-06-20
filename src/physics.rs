371af7097778789f26c1cb05c931ef1f37cc4ce2
Salmaan Saeed <salsaeed999@gmail.com>
2023-06-20 01:43:10 -0400

    avoided using magic numbers in activate collision function

--- src/physics.rs
+++ src/physics.rs
@@ -4,25 +4,25 @@ use crate::object;
 pub fn activate_collision(player: &mut object::Player<'_>, object: &object::Object<'_>) {
     /* Player and Object collision */
     let vertex_gap: i32 = 2;
-    if player.dstrect.y < object.dstrect.y + 50 - vertex_gap && player.dstrect.y + 50 > object.dstrect.y + vertex_gap {
-        if player.dstrect.x + 50 > object.dstrect.x && player.dstrect.x < object.dstrect.x + 25 {
+    if player.dstrect.y < object.dstrect.y + object.srcrect.h - vertex_gap && player.dstrect.y + player.srcrect.h > object.dstrect.y + vertex_gap {
+        if player.dstrect.x + player.srcrect.w > object.dstrect.x && player.dstrect.x < object.dstrect.x + object.srcrect.w / 2 {
             // left collision
-            player.dstrect.x = object.dstrect.x - 50; 
+            player.dstrect.x = object.dstrect.x - player.srcrect.w; 
         }
-        else if player.dstrect.x < object.dstrect.x + 50 && player.dstrect.x > object.dstrect.x + 25 {
+        else if player.dstrect.x < object.dstrect.x + object.srcrect.w && player.dstrect.x > object.dstrect.x + object.srcrect.w / 2 {
             // right collision
-            player.dstrect.x = object.dstrect.x + 50; 
+            player.dstrect.x = object.dstrect.x + object.srcrect.w; 
         }
     }
 
-    else if player.dstrect.x < object.dstrect.x - vertex_gap + 50 && player.dstrect.x + 50 > object.dstrect.x + vertex_gap {
-        if player.dstrect.y + 50 > object.dstrect.y && player.dstrect.y < object.dstrect.y + 25 {
+    else if player.dstrect.x < object.dstrect.x - vertex_gap + object.srcrect.w && player.dstrect.x + player.srcrect.w > object.dstrect.x + vertex_gap {
+        if player.dstrect.y + player.srcrect.h > object.dstrect.y && player.dstrect.y < object.dstrect.y + object.srcrect.h / 2 {
             // top collision
-            player.dstrect.y = object.dstrect.y - 50;
+            player.dstrect.y = object.dstrect.y - player.srcrect.h;
         }
-        else if player.dstrect.y < object.dstrect.y + 50 && player.dstrect.y > object.dstrect.y + 25 {
+        else if player.dstrect.y < object.dstrect.y + object.srcrect.h && player.dstrect.y > object.dstrect.y + object.srcrect.h / 2 {
             // bottom collision
-            player.dstrect.y = object.dstrect.y + 50;
+            player.dstrect.y = object.dstrect.y + object.srcrect.h;
         }    
     }
 }
