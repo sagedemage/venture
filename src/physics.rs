d4956b6c2a6b5961e111569928d4e9954f79c100
Salmaan Saeed <salsaeed999@gmail.com>
2023-06-21 23:55:21 -0400

    improve the readability of the activate collision function

--- src/physics.rs
+++ src/physics.rs
@@ -7,25 +7,31 @@ pub fn activate_collision(player: &mut object::Player<'_>, object: &object::Obje
     let player_width: i32 = player.get_srcrect().w;
     let player_height: i32 = player.get_srcrect().h;
 
-    if player.dstrect.y < object.dstrect.y + object.srcrect.h - vertex_gap && player.dstrect.y + player.get_srcrect().h > object.dstrect.y + vertex_gap {
-        if player.dstrect.x + player_width > object.dstrect.x && player.dstrect.x < object.dstrect.x + object.srcrect.w / 2 {
+    /* Object's position */
+    let player_bottom_side: i32 = player.dstrect.y + player_height;
+    let player_right_side: i32 = player.dstrect.x + player_width;
+    let object_bottom_side: i32 = object.dstrect.y + object.srcrect.h;
+    let object_right_side: i32 = object.dstrect.x + object.srcrect.w;
+
+    if player.dstrect.y < object_bottom_side - vertex_gap && player_bottom_side > object.dstrect.y + vertex_gap {
+        if player_right_side > object.dstrect.x && player.dstrect.x < object.dstrect.x + object.srcrect.w / 2 {
             // left collision
             player.dstrect.x = object.dstrect.x - player_width; 
         }
-        else if player.dstrect.x < object.dstrect.x + object.srcrect.w && player.dstrect.x > object.dstrect.x + object.srcrect.w / 2 {
+        else if player.dstrect.x < object_right_side && player.dstrect.x > object.dstrect.x + object.srcrect.w / 2 {
             // right collision
-            player.dstrect.x = object.dstrect.x + object.srcrect.w; 
+            player.dstrect.x = object_right_side; 
         }
     }
 
-    else if player.dstrect.x < object.dstrect.x - vertex_gap + object.srcrect.w && player.dstrect.x + player_width > object.dstrect.x + vertex_gap {
-        if player.dstrect.y + player_height > object.dstrect.y && player.dstrect.y < object.dstrect.y + object.srcrect.h / 2 {
+    else if player.dstrect.x < object.dstrect.x - vertex_gap + object.srcrect.w && player_right_side > object.dstrect.x + vertex_gap {
+        if player_bottom_side > object.dstrect.y && player.dstrect.y < object.dstrect.y + object.srcrect.h / 2 {
             // top collision
             player.dstrect.y = object.dstrect.y - player_height;
         }
-        else if player.dstrect.y < object.dstrect.y + object.srcrect.h && player.dstrect.y > object.dstrect.y + object.srcrect.h / 2 {
+        else if player.dstrect.y < object_bottom_side && player.dstrect.y > object.dstrect.y + object.srcrect.h / 2 {
             // bottom collision
-            player.dstrect.y = object.dstrect.y + object.srcrect.h;
+            player.dstrect.y = object_bottom_side;
         }    
     }
 }
