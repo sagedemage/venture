77f7845bc572252c847e9e350159424b189cba76
Salmaan Saeed <salsaeed999@gmail.com>
2023-06-23 23:53:25 -0400

    created signed integer dimension variables and formatted the code

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
 extern crate sdl2;
 
-use sdl2::{event, keyboard, pixels, rect, render, video, mixer};
-use sdl2::image::{self, LoadTexture};
-use std::{time, path};
+use sdl2::image::LoadTexture;
+use sdl2::{event, image, keyboard, mixer, pixels, rect, render, video};
+use std::{path, time};
 
 mod object;
 mod physics;
 
-const LEVEL_WIDTH: i32 = 1280;
-const LEVEL_HEIGHT: i32 = 720;
-const PLAYER_WIDTH: i32 = 20;
-const PLAYER_HEIGHT: i32 = 20;
-const TREE_WIDTH: i32 = 40;
-const TREE_HEIGHT: i32 = 40;
+const LEVEL_WIDTH: u32 = 1280;
+const LEVEL_HEIGHT: u32 = 720;
+const PLAYER_WIDTH: u32 = 20;
+const PLAYER_HEIGHT: u32 = 20;
+const TREE_WIDTH: u32 = 40;
+const TREE_HEIGHT: u32 = 40;
 
 fn main() -> Result<(), String> {
     /* Run the Game */
     // Variables
+    let level_width: i32 = LEVEL_WIDTH as i32;
+    let level_height: i32 = LEVEL_HEIGHT as i32;
+    let player_width: i32 = PLAYER_WIDTH as i32;
+    let player_height: i32 = PLAYER_HEIGHT as i32;
+    let tree_width: i32 = TREE_WIDTH as i32;
+    let tree_height: i32 = TREE_HEIGHT as i32;
+
     let chunksize: i32 = 1024;
     let volume: i32 = 64; // 128 is max
     let fps: u32 = 60;
@@ -31,7 +38,12 @@ fn main() -> Result<(), String> {
     let video_subsystem: sdl2::VideoSubsystem = sdl.video()?;
     let _image_context: image::Sdl2ImageContext = image::init(image::InitFlag::PNG)?;
 
-    mixer::open_audio(mixer::DEFAULT_FREQUENCY, mixer::DEFAULT_FORMAT, mixer::DEFAULT_CHANNELS, chunksize)?;
+    mixer::open_audio(
+        mixer::DEFAULT_FREQUENCY,
+        mixer::DEFAULT_FORMAT,
+        mixer::DEFAULT_CHANNELS,
+        chunksize,
+    )?;
 
     let _mixer_contennt: mixer::Sdl2MixerContext = mixer::init(mixer::InitFlag::OGG)?;
 
@@ -39,7 +51,7 @@ fn main() -> Result<(), String> {
     mixer::Music::set_volume(volume);
 
     let window: video::Window = video_subsystem
-        .window("venture", LEVEL_WIDTH as u32, LEVEL_HEIGHT as u32)
+        .window("venture", LEVEL_WIDTH, LEVEL_HEIGHT)
         .position_centered()
         .build()
         .map_err(|e| e.to_string())?;
@@ -51,15 +63,21 @@ fn main() -> Result<(), String> {
         .map_err(|e| e.to_string())?;
 
     // texture loader
-    let texture_creator: render::TextureCreator<sdl2::video::WindowContext> = canvas.texture_creator();
+    let texture_creator: render::TextureCreator<sdl2::video::WindowContext> =
+        canvas.texture_creator();
     let player_texture: render::Texture<'_> = texture_creator.load_texture(player_image_path)?;
 
-    let player_srcrect: rect::Rect = rect::Rect::new(0, 0, PLAYER_WIDTH as u32, PLAYER_HEIGHT as u32);
-    let player_dstrect: rect::Rect =  rect::Rect::new(LEVEL_WIDTH / 4 - PLAYER_WIDTH / 2, LEVEL_HEIGHT / 2 - PLAYER_HEIGHT / 2, PLAYER_WIDTH as u32, PLAYER_HEIGHT as u32);
+    let player_srcrect: rect::Rect = rect::Rect::new(0, 0, PLAYER_WIDTH, PLAYER_HEIGHT);
+    let player_dstrect: rect::Rect = rect::Rect::new(
+        level_width / 4 - player_width / 2,
+        level_height / 2 - player_height / 2,
+        PLAYER_WIDTH,
+        PLAYER_HEIGHT,
+    );
 
     let mut player: object::Player<'_> = object::Player::new(
         &player_texture,
-        player_speed, 
+        player_speed,
         player_srcrect,
         player_dstrect,
     );
@@ -70,87 +88,157 @@ fn main() -> Result<(), String> {
     // trees near the top of the window
     let tree1: object::Object<'_> = object::Object {
         texture: &tree_texture,
-        srcrect: rect::Rect::new(0, 0, TREE_WIDTH as u32, TREE_HEIGHT as u32),
-        dstrect: rect::Rect::new(LEVEL_WIDTH / 8 - TREE_WIDTH / 2, LEVEL_HEIGHT / 4 - TREE_HEIGHT / 2, TREE_WIDTH as u32, TREE_HEIGHT as u32),
+        srcrect: rect::Rect::new(0, 0, TREE_WIDTH, TREE_HEIGHT),
+        dstrect: rect::Rect::new(
+            level_width / 8 - tree_width / 2,
+            level_height / 4 - tree_height / 2,
+            TREE_WIDTH,
+            TREE_HEIGHT,
+        ),
     };
 
     let tree2: object::Object<'_> = object::Object {
         texture: &tree_texture,
-        srcrect: rect::Rect::new(0, 0, TREE_WIDTH as u32, TREE_HEIGHT as u32),
-        dstrect: rect::Rect::new(LEVEL_WIDTH / 4 - TREE_WIDTH / 2, LEVEL_HEIGHT / 4 - TREE_HEIGHT / 2, TREE_WIDTH as u32, TREE_HEIGHT as u32),
+        srcrect: rect::Rect::new(0, 0, TREE_WIDTH, TREE_HEIGHT),
+        dstrect: rect::Rect::new(
+            level_width / 4 - tree_width / 2,
+            level_height / 4 - tree_height / 2,
+            TREE_WIDTH,
+            TREE_HEIGHT,
+        ),
     };
 
     let tree3: object::Object<'_> = object::Object {
         texture: &tree_texture,
-        srcrect: rect::Rect::new(0, 0, TREE_WIDTH as u32, TREE_HEIGHT as u32),
-        dstrect: rect::Rect::new(LEVEL_WIDTH * 3 / 8 - TREE_WIDTH / 2, LEVEL_HEIGHT / 4 - TREE_HEIGHT / 2, TREE_WIDTH as u32, TREE_HEIGHT as u32),
+        srcrect: rect::Rect::new(0, 0, TREE_WIDTH, TREE_HEIGHT),
+        dstrect: rect::Rect::new(
+            level_width * 3 / 8 - tree_width / 2,
+            level_height / 4 - tree_height / 2,
+            TREE_WIDTH,
+            TREE_HEIGHT,
+        ),
     };
 
     let tree4: object::Object<'_> = object::Object {
         texture: &tree_texture,
-        srcrect: rect::Rect::new(0, 0, TREE_WIDTH as u32, TREE_HEIGHT as u32),
-        dstrect: rect::Rect::new(LEVEL_WIDTH / 2 - TREE_WIDTH / 2, LEVEL_HEIGHT / 4 - TREE_HEIGHT / 2, TREE_WIDTH as u32, TREE_HEIGHT as u32),
+        srcrect: rect::Rect::new(0, 0, TREE_WIDTH, TREE_HEIGHT),
+        dstrect: rect::Rect::new(
+            level_width / 2 - tree_width / 2,
+            level_height / 4 - tree_height / 2,
+            TREE_WIDTH,
+            TREE_HEIGHT,
+        ),
     };
 
     let tree5: object::Object<'_> = object::Object {
         texture: &tree_texture,
-        srcrect: rect::Rect::new(0, 0, TREE_WIDTH as u32, TREE_HEIGHT as u32),
-        dstrect: rect::Rect::new(LEVEL_WIDTH * 5 / 8 - TREE_WIDTH / 2, LEVEL_HEIGHT / 4 - TREE_HEIGHT / 2, TREE_WIDTH as u32, TREE_HEIGHT as u32),
+        srcrect: rect::Rect::new(0, 0, TREE_WIDTH, TREE_HEIGHT),
+        dstrect: rect::Rect::new(
+            level_width * 5 / 8 - tree_width / 2,
+            level_height / 4 - tree_height / 2,
+            TREE_WIDTH,
+            TREE_HEIGHT,
+        ),
     };
 
     let tree6: object::Object<'_> = object::Object {
         texture: &tree_texture,
-        srcrect: rect::Rect::new(0, 0, TREE_WIDTH as u32, TREE_HEIGHT as u32),
-        dstrect: rect::Rect::new(LEVEL_WIDTH * 3 / 4 - TREE_WIDTH / 2, LEVEL_HEIGHT / 4 - TREE_HEIGHT / 2, TREE_WIDTH as u32, TREE_HEIGHT as u32),
+        srcrect: rect::Rect::new(0, 0, TREE_WIDTH, TREE_HEIGHT),
+        dstrect: rect::Rect::new(
+            level_width * 3 / 4 - tree_width / 2,
+            level_height / 4 - tree_height / 2,
+            TREE_WIDTH,
+            TREE_HEIGHT,
+        ),
     };
 
     let tree7: object::Object<'_> = object::Object {
         texture: &tree_texture,
-        srcrect: rect::Rect::new(0, 0, TREE_WIDTH as u32, TREE_HEIGHT as u32),
-        dstrect: rect::Rect::new(LEVEL_WIDTH * 7 / 8 - TREE_WIDTH / 2, LEVEL_HEIGHT / 4 - TREE_HEIGHT / 2, TREE_WIDTH as u32, TREE_HEIGHT as u32),
+        srcrect: rect::Rect::new(0, 0, TREE_WIDTH, TREE_HEIGHT),
+        dstrect: rect::Rect::new(
+            level_width * 7 / 8 - tree_width / 2,
+            level_height / 4 - tree_height / 2,
+            TREE_WIDTH,
+            TREE_HEIGHT,
+        ),
     };
 
     // trees near the bottom of the window
     let tree8: object::Object<'_> = object::Object {
         texture: &tree_texture,
-        srcrect: rect::Rect::new(0, 0, TREE_WIDTH as u32, TREE_HEIGHT as u32),
-        dstrect: rect::Rect::new(LEVEL_WIDTH / 8 -TREE_WIDTH / 2, LEVEL_HEIGHT * 3 / 4 - TREE_HEIGHT / 2, TREE_WIDTH as u32, TREE_HEIGHT as u32),
+        srcrect: rect::Rect::new(0, 0, TREE_WIDTH, TREE_HEIGHT),
+        dstrect: rect::Rect::new(
+            level_width / 8 - tree_width / 2,
+            level_height * 3 / 4 - tree_height / 2,
+            TREE_WIDTH,
+            TREE_HEIGHT,
+        ),
     };
 
     let tree9: object::Object<'_> = object::Object {
         texture: &tree_texture,
-        srcrect: rect::Rect::new(0, 0, TREE_WIDTH as u32, TREE_HEIGHT as u32),
-        dstrect: rect::Rect::new(LEVEL_WIDTH / 4 - TREE_WIDTH / 2, LEVEL_HEIGHT * 3 / 4 - TREE_HEIGHT / 2, TREE_WIDTH as u32, TREE_HEIGHT as u32),
+        srcrect: rect::Rect::new(0, 0, TREE_WIDTH, TREE_HEIGHT),
+        dstrect: rect::Rect::new(
+            level_width / 4 - tree_width / 2,
+            level_height * 3 / 4 - tree_height / 2,
+            TREE_WIDTH,
+            TREE_HEIGHT,
+        ),
     };
 
     let tree10: object::Object<'_> = object::Object {
         texture: &tree_texture,
-        srcrect: rect::Rect::new(0, 0, TREE_WIDTH as u32, TREE_HEIGHT as u32),
-        dstrect: rect::Rect::new(LEVEL_WIDTH * 3 / 8 - TREE_WIDTH / 2, LEVEL_HEIGHT * 3 / 4 - TREE_HEIGHT / 2, TREE_WIDTH as u32, TREE_HEIGHT as u32),
+        srcrect: rect::Rect::new(0, 0, TREE_WIDTH, TREE_HEIGHT),
+        dstrect: rect::Rect::new(
+            level_width * 3 / 8 - tree_width / 2,
+            level_height * 3 / 4 - tree_height / 2,
+            TREE_WIDTH,
+            TREE_HEIGHT,
+        ),
     };
 
     let tree11: object::Object<'_> = object::Object {
         texture: &tree_texture,
-        srcrect: rect::Rect::new(0, 0, TREE_WIDTH as u32, TREE_HEIGHT as u32),
-        dstrect: rect::Rect::new(LEVEL_WIDTH / 2 - TREE_WIDTH / 2, LEVEL_HEIGHT * 3 / 4 - TREE_HEIGHT / 2, TREE_WIDTH as u32, TREE_HEIGHT as u32),
+        srcrect: rect::Rect::new(0, 0, TREE_WIDTH, TREE_HEIGHT),
+        dstrect: rect::Rect::new(
+            level_width / 2 - tree_width / 2,
+            level_height * 3 / 4 - tree_height / 2,
+            TREE_WIDTH,
+            TREE_HEIGHT,
+        ),
     };
 
     let tree12: object::Object<'_> = object::Object {
         texture: &tree_texture,
-        srcrect: rect::Rect::new(0, 0, TREE_WIDTH as u32, TREE_HEIGHT as u32),
-        dstrect: rect::Rect::new(LEVEL_WIDTH * 5 / 8 - TREE_WIDTH / 2, LEVEL_HEIGHT * 3 / 4 - TREE_HEIGHT / 2, TREE_WIDTH as u32, TREE_HEIGHT as u32),
+        srcrect: rect::Rect::new(0, 0, TREE_WIDTH, TREE_HEIGHT),
+        dstrect: rect::Rect::new(
+            level_width * 5 / 8 - tree_width / 2,
+            level_height * 3 / 4 - tree_height / 2,
+            TREE_WIDTH,
+            TREE_HEIGHT,
+        ),
     };
 
     let tree13: object::Object<'_> = object::Object {
         texture: &tree_texture,
-        srcrect: rect::Rect::new(0, 0, TREE_WIDTH as u32, TREE_HEIGHT as u32),
-        dstrect: rect::Rect::new(LEVEL_WIDTH * 3 / 4 - TREE_WIDTH / 2, LEVEL_HEIGHT * 3 / 4 - TREE_HEIGHT / 2, TREE_WIDTH as u32, TREE_HEIGHT as u32),
+        srcrect: rect::Rect::new(0, 0, TREE_WIDTH, TREE_HEIGHT),
+        dstrect: rect::Rect::new(
+            level_width * 3 / 4 - tree_width / 2,
+            level_height * 3 / 4 - tree_height / 2,
+            TREE_WIDTH,
+            TREE_HEIGHT,
+        ),
     };
 
     let tree14: object::Object<'_> = object::Object {
         texture: &tree_texture,
-        srcrect: rect::Rect::new(0, 0, TREE_WIDTH as u32, TREE_HEIGHT as u32),
-        dstrect: rect::Rect::new(LEVEL_WIDTH * 7 / 8 - TREE_WIDTH / 2, LEVEL_HEIGHT * 3 / 4 - TREE_HEIGHT / 2, TREE_WIDTH as u32, TREE_HEIGHT as u32),
+        srcrect: rect::Rect::new(0, 0, TREE_WIDTH, TREE_HEIGHT),
+        dstrect: rect::Rect::new(
+            level_width * 7 / 8 - tree_width / 2,
+            level_height * 3 / 4 - tree_height / 2,
+            TREE_WIDTH,
+            TREE_HEIGHT,
+        ),
     };
 
     /* Plays the music theme forever */
@@ -162,10 +250,10 @@ fn main() -> Result<(), String> {
         /* Click Keybindings */
         for event in sdl.event_pump()?.poll_iter() {
             match event {
-                event::Event::Quit {..} 
-                | event::Event::KeyDown { 
+                event::Event::Quit { .. }
+                | event::Event::KeyDown {
                     keycode: Option::Some(keyboard::Keycode::Escape),
-                    .. 
+                    ..
                 } => break 'running,
                 _ => {}
             }
@@ -175,19 +263,31 @@ fn main() -> Result<(), String> {
         let event: sdl2::EventPump = sdl.event_pump()?;
 
         /* Player Movement Keybindings */
-        if event.keyboard_state().is_scancode_pressed(keyboard::Scancode::Right) {
+        if event
+            .keyboard_state()
+            .is_scancode_pressed(keyboard::Scancode::Right)
+        {
             // player moves right
             player.dstrect.x += player.get_speed();
         }
-        if event.keyboard_state().is_scancode_pressed(keyboard::Scancode::Left) {
+        if event
+            .keyboard_state()
+            .is_scancode_pressed(keyboard::Scancode::Left)
+        {
             // player moves left
             player.dstrect.x -= player.get_speed();
         }
-        if event.keyboard_state().is_scancode_pressed(keyboard::Scancode::Up) {
+        if event
+            .keyboard_state()
+            .is_scancode_pressed(keyboard::Scancode::Up)
+        {
             // player moves up
             player.dstrect.y -= player.get_speed();
         }
-        if event.keyboard_state().is_scancode_pressed(keyboard::Scancode::Down) {
+        if event
+            .keyboard_state()
+            .is_scancode_pressed(keyboard::Scancode::Down)
+        {
             // player moves down
             player.dstrect.y += player.get_speed();
         }
@@ -213,13 +313,13 @@ fn main() -> Result<(), String> {
             // left boundary
             player.dstrect.x = 0;
         }
-        if player.dstrect.x + player.dstrect.w > LEVEL_WIDTH {
+        if player.dstrect.x + player.dstrect.w > level_width {
             // right boundary
-            player.dstrect.x = LEVEL_WIDTH - player.dstrect.w;
+            player.dstrect.x = level_width - player.dstrect.w;
         }
-        if player.dstrect.y + player.dstrect.h > LEVEL_HEIGHT {
+        if player.dstrect.y + player.dstrect.h > level_height {
             // bottom boundary
-            player.dstrect.y = LEVEL_HEIGHT - player.dstrect.h;
+            player.dstrect.y = level_height - player.dstrect.h;
         }
         if player.dstrect.y < 0 {
             // top boundary
